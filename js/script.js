aab070564552a92508987d27c282bdc2a4362ae6
Mahadi92 <mahadi.hasan.937524.mh@gmail.com>
2021-01-30 00:59:23 +0600

    Increase and decrease added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-console.log("It works");
+document.getElementById('first-class-plus').addEventListener('click', function () {
+    const quantityInput = document.getElementById('quantity-input');
+    const quantityCount = parseInt(quantityInput.value);
+    const quantityNewCount = quantityCount + 1;
+    quantityInput.value = quantityNewCount;
+    const priceTotal = quantityNewCount * 150;
+    console.log(priceTotal);
+})
+
+document.getElementById('first-class-minus').addEventListener('click', function () {
+    const quantityInput = document.getElementById('quantity-input');
+    const quantityCount = parseInt(quantityInput.value);
+    const quantityNewCount = quantityCount - 1;
+    quantityInput.value = quantityNewCount;
+    const priceTotal = quantityNewCount * 150;
+    console.log(priceTotal);
+})
+
+function handleClassChanges() {
+
+}
